2927100ab5de0c621ba7e3069aa260958b9e0998
Varunan_V <47948397+varunan-vara@users.noreply.github.com>
2020-09-10 20:07:00 -0400

    Created React Native Project for Windows

---
+++ windows/HTMLEditor/MainPage.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+
+namespace winrt::HTMLEditor::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::HTMLEditor::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
+
